0c5d999c21d781e145de54649f2cbdf3ebd575a5
IgorMontezuma20 <igorminranda17@gmail.com>
2023-01-29 12:01:27 -0300

    added code style configs

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 
-
 const server = express();
 
-server.get('/', (req, res) =>{
 
-    return res.send('Olá, Dev!')
+interface Teste{
+
+}
+
+server.get('/', (req, res) => {
+  return res.send('Olá, Dev!');
 });
-
-
 
 export { server };
